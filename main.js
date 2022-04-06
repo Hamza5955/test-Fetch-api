fdb31cd6a8b0d2f9d631678918ea865f3a5860ee
Hamza <Hamza5955>
2022-04-07 01:37:01 +0200

    funge

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ console.log("ciao mondo");
 document.getElementById("myBtn").addEventListener("click", getData);
 
 function getData() {
-  fetch("https://randomuser.me/api/?results=10").then((data) => {
-    let author = data.results;
-  });
+  fetch("https://randomuser.me/api/?results=10")
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log("data");
+      let author = data.results;
+      console.log(author);
+    });
 }
