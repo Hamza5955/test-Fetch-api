bc490d9683a11124c9c2f8c01df466f78fcd6bc1
Hamza <Hamza5955>
2022-04-07 01:30:23 +0200

    ?????

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("ciaoo mondo");
+console.log("ciao mondo");
+
+document.getElementById("myBtn").addEventListener("click", getData);
+
+function getData() {
+  fetch("https://randomuser.me/api/?results=10").then((data) => {
+    let author = data.results;
+  });
+}
